f5ce2bdb75336d8cf675b42c0b06da25b811ca38
natasha-robarge <robarge.natasha7@gmail.com>
2020-09-28 19:37:31 -0500

    after 3 hours, I finally got the hamburg slide to work (x.x) dying penguin on the inside

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Icon from '@material-ui/core/Icon';
+import MenuIcon from '@material-ui/icons/Menu';
 import '../styles/Navbar.css';
 
-const Navbar = () => (
-  <div className="navbarContainer">
-    <h4><a href="/">Home</a></h4>
-    <div className="navbarItemContainer">
-      <h4><a href="#">About</a></h4>
-      <h4><a href="#">Resume</a></h4>
-      <h4><a href="#">Projects</a></h4>
-      <h4><a href="#">Contact</a></h4>
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  console.log(isOpen, ' is open?')
+
+  return (
+    <div className="navbarContainer">
+      {/* <h4><a href="/">Home</a></h4> */}
+      <div>
+        <button className="navButton" type="button" onClick={() => !isOpen ? setOpen(true) : setOpen(false)}>
+          <Icon className="menuIcon">{!isOpen ? 'menu' : 'close'}</Icon>
+        </button>
+        <div className={`navbarItemContainer ${isOpen ? 'visible' : 'closed'}`}>
+          <h4><a href="#about" onClick={() => setOpen(false)}>About</a></h4>
+          <h4><a href="#" onClick={() => setOpen(false)}>Resume</a></h4>
+          <h4><a href="#" onClick={() => setOpen(false)}>Projects</a></h4>
+          <h4><a href="#" onClick={() => setOpen(false)}>Contact</a></h4>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Navbar;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import '../styles/About.css';
 
 const About = () => {
   return (
-    <div>
+    <div className="aboutContainer">
+      <div>
+        <h1 id="about">About</h1>
+      </div>
       <h2>I'm Nat and I'm a Software Engineer</h2>
     </div>
   )
