4f6f605e52a49de07e0e697b8a256bf907a97c28
natasha-robarge <robarge.natasha7@gmail.com>
2021-01-05 16:17:34 -0600

    add logo image through hosting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="homeContainer">
-      <img src={require('../assets/images/default.png')} className="logo-image" alt="logo" aria-label="Natasha Tullos, Software Engineer" />
+      <img src="https://i.maga.host/QBqkMiB.png" className="logo-image" alt="logo" aria-label="Natasha Tullos, Software Engineer" />
     </div>
   )
 }
